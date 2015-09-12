dc6a64145e7ad10b1cbc02b08ec44bd2094721ab
eric360 <eric-richard.hongtuanha@360learning.com>
2015-09-12 23:00:39 +0200

    Initial Commit

---
+++ MemoTests/MemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MemoTests.swift
+//  MemoTests
+//
+//  Created by eric_360 on 12/09/15.
+//  Copyright (c) 2015 Eric Hong tuan ha. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
