29a102ebeb38e13f2d2f773a7309779a4e79a37b
eric360 <eric-richard.hongtuanha@360learning.com>
2015-09-14 09:08:32 +0200

    Location manager added

--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        var locationManager : CLLocationManager =  CLLocationManager()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = 100
+        locationManager.distanceFilter = 10
+        locationManager.startUpdatingLocation()
     }
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+    }
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println(locations)
     }
 }
 
